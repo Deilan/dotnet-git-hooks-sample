1674b34d86b5ae02f2db0d2cf61cc4a7ad81df86
Deilan <deilan@users.noreply.github.com>
2018-07-17 19:17:09 +0300

    test: add sample test

---
+++ GitHooksSampleConsoleApp.UnitTests/SampleUnitTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GitHooksSampleConsoleApp.UnitTests
+{
+    public class SampleUnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
